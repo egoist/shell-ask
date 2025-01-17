ad480479a9ac1f63be329fe2cb767df7bd2d1d3a
EGOIST <hi@egoist.dev>
2025-01-17 16:26:53 +0800

    inject platform to context

--- src/ask.ts
+++ src/ask.ts
@@ -32,7 +32,7 @@ export async function ask(
     stream?: boolean
     reply?: boolean
     breakdown?: boolean
-  },
+  }
 ) {
   if (!prompt) {
     throw new CliError("please provide a prompt")
@@ -52,8 +52,8 @@ export async function ask(
     modelId === "select"
       ? true
       : modelId === "ollama" || modelId.startsWith("ollama-")
-        ? "required"
-        : false,
+      ? "required"
+      : false
   )
 
   if (
@@ -63,7 +63,7 @@ export async function ask(
   ) {
     if (process.platform === "win32" && !process.stdin.isTTY) {
       throw new CliError(
-        "Interactively selecting a model is not supported on Windows when using piped input. Consider directly specifying the model id instead, for example: `-m gpt-4o`",
+        "Interactively selecting a model is not supported on Windows when using piped input. Consider directly specifying the model id instead, for example: `-m gpt-4o`"
       )
     }
 
@@ -85,7 +85,7 @@ export async function ask(
 
         choices: models
           .filter(
-            (item) => modelId === "select" || item.id.startsWith(`${modelId}-`),
+            (item) => modelId === "select" || item.id.startsWith(`${modelId}-`)
           )
           .map((item) => {
             return {
@@ -106,13 +106,13 @@ export async function ask(
   debug(`Selected modelID: ${modelId}`)
 
   const matchedModel = models.find(
-    (m) => m.id === modelId || m.realId === modelId,
+    (m) => m.id === modelId || m.realId === modelId
   )
   if (!matchedModel) {
     throw new CliError(
       `model not found: ${modelId}\n\navailable models: ${models
         .map((m) => m.id)
-        .join(", ")}`,
+        .join(", ")}`
     )
   }
   const realModelId = matchedModel.realId || modelId
@@ -125,7 +125,8 @@ export async function ask(
   const context = [
     // inhert prev chat
     !chat && `Context:`,
-    !chat && `shell: ${process.env.SHELL || "unknown"}`,
+    !chat &&
+      `platform: ${process.platform}\nshell: ${process.env.SHELL || "unknown"}`,
 
     options.pipeInput && [`stdin:`, "```", options.pipeInput, "```"].join("\n"),
 
@@ -134,7 +135,7 @@ export async function ask(
 
     remoteContents.length > 0 && "remote contents:",
     ...remoteContents.map(
-      (content) => `${content.url}:\n"""\n${content.content}\n"""`,
+      (content) => `${content.url}:\n"""\n${content.content}\n"""`
     ),
   ]
     .filter(notEmpty)
